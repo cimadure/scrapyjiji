a19cc1aa1b63a9715211fb0344833a51379f118b
RONAN <ronan.cimadure@gmail.com>
2019-03-10 15:59:35 -0400

    dash with location data

--- kijiji/app.py
+++ kijiji/app.py
@@ -19,10 +19,31 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 df = pd.read_json('test/unclean_data.json', orient='records')
+df = df.dropna()
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
+
+    dcc.Graph(
+        id='basic-interactions',
+        figure={
+            'data': [
+                {
+                    'x': df['latitude'],
+                    'y': df['longitude'],
+                    'text': df['title'],
+                    'customdata': df['price'],
+                    'name': 'Kijiji',
+                    'mode': 'markers',
+                    'marker': {'size': 12}
+                },
+            ],
+            'layout': {
+                'clickmode': 'event+select'
+            }
+        }
+    ),
 
     dash_table.DataTable(
         id='table',
@@ -30,34 +51,7 @@ app.layout = html.Div([
         data=df.to_dict("rows"),
     ),
 
-    dcc.Graph(
-        id='basic-interactions',
-        figure={
-            'data': [
-                {
-                    'x': [1, 2, 3, 4],
-                    'y': [4, 1, 3, 5],
-                    'text': ['a', 'b', 'c', 'd'],
-                    'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
-                    'name': 'Trace 1',
-                    'mode': 'markers',
-                    'marker': {'size': 12}
-                },
-                {
-                    'x': [1, 2, 3, 4],
-                    'y': [9, 4, 1, 4],
-                    'text': ['w', 'x', 'y', 'z'],
-                    'customdata': ['c.w', 'c.x', 'c.y', 'c.z'],
-                    'name': 'Trace 2',
-                    'mode': 'markers',
-                    'marker': {'size': 12}
-                }
-            ],
-            'layout': {
-                'clickmode': 'event+select'
-            }
-        }
-    ),
+
 ])
 
 
