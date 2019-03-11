4179f5615e2b7486f1190b13fa9f9fdc178cc376
RONAN <ronan.cimadure@gmail.com>
2019-03-10 23:41:13 -0400

    visualize locaation and price on a map + data table :)

--- kijiji/app.py
+++ kijiji/app.py
@@ -1,15 +1,5 @@
 # # -*- coding: utf-8 -*-
-# import os
-# import dash
-#
-# app = dash.Dash(__name__)
-# server = app.server
-#
-# # Running the server
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
-
-
+from environs import Env
 import dash
 import dash_table
 import pandas as pd
@@ -18,42 +8,59 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 
+env = Env()
+env.read_env()  # read .env file, if it exists
+mapbox_access_token = env("MAPBOX_ACCESS_TOKEN")
+
 df = pd.read_json('test/unclean_data.json', orient='records')
 df = df.dropna()
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
+data = [go.Scattermapbox(
+    lat=list(df['latitude']),
+    lon=list(df['longitude']),
+    mode='markers',
+    marker=go.scattermapbox.Marker(size=8,
+                                   color='rgb(242, 177, 172)',
+                                   opacity=0.7),
+    text=df['price'],
+    hoverinfo='text'
+)]
 
-    dcc.Graph(
-        id='basic-interactions',
-        figure={
-            'data': [
-                {
-                    'x': df['latitude'],
-                    'y': df['longitude'],
-                    'text': df['title'],
-                    'customdata': df['price'],
-                    'name': 'Kijiji',
-                    'mode': 'markers',
-                    'marker': {'size': 12}
-                },
-            ],
-            'layout': {
-                'clickmode': 'event+select'
-            }
-        }
+layout = go.Layout(
+    title='Kijiji Rooms',
+    autosize=True,
+    hovermode='closest',
+    showlegend=False,
+    mapbox=go.layout.Mapbox(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=go.layout.mapbox.Center(lat=45.92, lon=-73.07),
+        pitch=1,
+        zoom=10,
+        style='light'
     ),
+)
 
-    dash_table.DataTable(
-        id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict("rows"),
-    ),
+fig = go.Figure(data=data, layout=layout)
 
+app.layout = html.Div(children=[
+                                html.H1(children='Hello Scraper'),
 
-])
+                                html.Div(children='''
+                                    Dash: A web application framework for Python.
+                                '''),
+                                dcc.Graph(
+                                    id='example-graph',
+                                    figure=fig),
 
+                                dash_table.DataTable(
+                                    id='table',
+                                    columns=[{"name": i, "id": i} for i in df.columns],
+                                    data=df.to_dict("rows"),
+                                ),
+                                ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
