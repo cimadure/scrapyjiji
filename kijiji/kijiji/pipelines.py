40613e957ad02d6766b03d0066cbde820b387d0f
RONAN <ronan.cimadure@gmail.com>
2019-03-13 12:57:17 -0400

    duplicates filter

--- kijiji/kijiji/pipelines.py
+++ kijiji/kijiji/pipelines.py
@@ -5,7 +5,17 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exceptions import DropItem
+
 
 class KijijiPipeline(object):
+
+    def __init__(self):
+        self.ids_seen = set()
+
     def process_item(self, item, spider):
-        return item
+        if item['ad_id'] in self.ids_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.ids_seen.add(item['ad_id'])
+            return item
